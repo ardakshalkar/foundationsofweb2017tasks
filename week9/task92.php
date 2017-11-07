f86abaca105c06ac4dce3f6b8a1a88b6ca749523
ardak <ardak.shalkar@gmail.com>
2017-11-07 16:34:21 +0600

    Task 9 added

---
+++ week9/task92.php
@@ -0,0 +1,10 @@
+
+<a href="task92.php?category=sport">Sport news</a> | <a href="task92.php?category=politics">Politic news</a><br/><br/>
+<a href="task92.php?category=sport&format=json">Sport news (JSON)</a> | <a href="task92.php?category=politics&format=json">Politic news (JSON)</a><br/><br/>
+
+<?php
+$news = ["sport"=>["C. Ronaldo has scored three goals in last five matches","Golovkin has won match for title"],"politics"=>["Trump has cancelled his visit to North Corea, because of sanction","N. Nazarbayev has approved new version of alphabet"]];
+$category = "sport"];
+
+/* Write your code here */
+?>
